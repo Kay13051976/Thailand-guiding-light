194eb416dcf15b32befc590e4ba345dadd095805
Wanwisa Waddington <kaywanwisa4@googlemail.com>
2023-12-30 17:17:15 +0000

    Create class register in models.py file to create database and admin user

--- usersapp/models.py
+++ usersapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Register(models.Model):
+    username = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=50)
