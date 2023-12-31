368ebf49f04e08249753f1151f13d0f2ac84019f
Wanwisa Waddington <kaywanwisa4@googlemail.com>
2023-12-31 16:08:50 +0000

    Create admistrator app in social_media_project folder

--- usersapp/models.py
+++ usersapp/models.py
@@ -6,3 +6,8 @@ class Register(models.Model):
     username = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=50)
+
+# class YourAccount(models.Model):
+#     your_account_id = ShortUUIDField(length=7, max_length=25, alphabet='abcdefghijklmnopqrstuvwxyz')
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     avatar = CloudinaryField('avatar', folder='avatars', blank=True, null=True)
