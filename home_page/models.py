f15bf42390feefb692cae90a308ccb335861bcc2
Wanwisa Waddington <kaywanwisa4@googlemail.com>
2024-01-01 13:33:16 +0000

    Create Class Post in medel.py in home_page app

--- home_page/models.py
+++ home_page/models.py
@@ -5,16 +5,22 @@ from cloudinary.models import CloudinaryField
 # Create your models here.
 STATUS = ((0, "Draft"), (1, "Published"))
 
-# class Post(models.Model):
-#     title = models.CharField(max_length=200, unique=True)
-#     slug = models.SlugField(max_length=200, unique=True)
-#     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_posts")
-#     updated_on = models.DateTimeField(auto_now=True)
-#     content = models.DateTimeField(auto_now=True)
-#     featured_image = CloudinaryField('image', default='placeholder')
-#     excerpt = models.TextField(blank=True)
-#     created_on = models.DateTimeField(auto_now_add=True)
-#     status = models.IntegerField(choices=STATUS, default=0)
-#     likes = models.ManyToManyField(User, related_name='user-likes', blank=True)
-# class Meta:
-#     ordering = ['-created_on']
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    connector = models.ForeignKey(User, on_delete=models.CASCADE, related_name="home_page_posts")
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.DateTimeField(auto_now=True)
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    heart = models.ManyToManyField(User, related_name='home_page_hearts', blank=True)
+class Meta:
+    ordering = ['-created_on']
+    
+def __str__(self):
+    return self.title
+
+def number_of_hearts(self):
+    return self.likes.count()
