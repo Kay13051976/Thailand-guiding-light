dc8466fe7e492ded9192dcc5cfe0c3b853ab4110
Wanwisa Waddington <kaywanwisa4@googlemail.com>
2024-01-01 06:06:22 +0000

    Register class Register from models.py file to admin.py file in usersapp

--- usersapp/admin.py
+++ usersapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-# from usersapp.models import Register
+from usersapp.models import Register
 
 # Register your models here.
-# admin.site.register(Register)
+admin.site.register(Register)
