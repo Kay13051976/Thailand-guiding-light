cfee66ee10e3377596a0d9c8c93b0496011efae8
Wanwisa Waddington <kaywanwisa4@googlemail.com>
2024-01-02 14:21:19 +0000

    Add list_display and search_fields property to PostAdmin in admin.py file in home app to display property to customize what we see in the list of view of the admin panel

--- home/admin.py
+++ home/admin.py
@@ -6,6 +6,8 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
     
+    list_display = ('title', 'status', 'created_on')
+    search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content')
 
