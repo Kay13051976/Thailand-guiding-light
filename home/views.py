925d2de8a8d817f4c1c01b6d505b4451465747dd
Wanwisa Waddington <kaywanwisa4@googlemail.com>
2024-01-03 12:59:02 +0000

    Create base layout.html file and build html structure

--- home/views.py
+++ home/views.py
@@ -4,13 +4,13 @@ from home.models import Post
 from django.template.response import TemplateResponse
 
 # class PostList(generic.ListView):
-#     model = Post
-#     queryset = Post.objects.filter(status=1).order_by('-created_on')
-#     template_name = 'home.html'
-#     paginate_by = 6
-    
+    # model = Post
+# queryset = Post.objects.filter(status = 1).order_by('-created_on')
+# template_name = 'home.html'
+# paginate_by = 6
+
 def home(request):
-    return render(request,"home.html")
+    return render(request, "home.html")
 
 def layout(request):
-    return render(request,"layout.html")
+    return render(request, "layout.html")
