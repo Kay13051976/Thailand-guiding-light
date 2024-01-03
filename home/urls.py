94b3ca0ee99fcfe0fd0abe0c03bcd411f5d3821c
Wanwisa Waddington <kaywanwisa4@googlemail.com>
2024-01-03 14:07:56 +0000

    Add url path in urls.py in home app

--- home/urls.py
+++ home/urls.py
@@ -2,8 +2,9 @@ from home import views
 from django.urls import path
 
 urlpatterns=[
-    path('home',views.home),
-    path('layout',views.layout),
+    path('home', views.PostList.as_view(), name='home')
+    # path('home',views.home),
+    # path('layout',views.layout),
     
     # path('login',views.login),
     # path('logout',views.logout)
--- home/views.py
+++ home/views.py
@@ -3,14 +3,14 @@ from django.views import generic
 from home.models import Post
 from django.template.response import TemplateResponse
 
-# class PostList(generic.ListView):
-    # model = Post
-# queryset = Post.objects.filter(status = 1).order_by('-created_on')
-# template_name = 'home.html'
-# paginate_by = 6
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status = 1).order_by('-created_on')
+    template_name = 'home.html'
+    paginate_by = 6
 
-def home(request):
-    return render(request, "home.html")
+# def home(request):
+#     return render(request, "home.html")
 
-def layout(request):
-    return render(request, "layout.html")
+# def layout(request):
+#     return render(request, "layout.html")
